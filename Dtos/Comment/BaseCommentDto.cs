f99143103219abcd7c65d5d4fc52a606f1acefeb
Aleksandr Babayan <babayanaleksandr80@gmail.com>
2025-06-13 00:24:53 +0400

    feat: Add validation for Dtos

--- Dtos/Comment/BaseCommentDto.cs
+++ Dtos/Comment/BaseCommentDto.cs
@@ -1,10 +1,18 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace api.Dtos.Comment
 {
     public class BaseCommentDto
     {
+        [Required]
+        [MinLength(5, ErrorMessage = "Title must be min 5 characters")]
+        [MaxLength(280, ErrorMessage = "Title cannot be over 280 characters")]
         public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(5, ErrorMessage = "Context must be min 5 characters")]
+        [MaxLength(280, ErrorMessage = "Context cannot be over 280 characters")]
         public string Content { get; set; } = string.Empty;
 
     }
--- Dtos/Stock/BaseStockDto.cs
+++ Dtos/Stock/BaseStockDto.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 using api.Dtos.Comment;
 
@@ -6,11 +7,31 @@ namespace api.Dtos.Stock
 {
     public class BaseStockDto
     {
+        [Required]
+        [MinLength(1, ErrorMessage = "Symbol must be min 1 characters")]
+        [MaxLength(10, ErrorMessage = "Symbol cannot be over 10 characters")]
         public string Symbol { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(1, ErrorMessage = "Company name must be min 1 characters")]
+        [MaxLength(20, ErrorMessage = "Company name cannot be over 20 characters")]
         public string CompanyName { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1, 1000000000)]
         public decimal Purchase { get; set; }
+
+        [Required]
+        [Range(0.001, 1000000000)]
         public decimal LastDiv { get; set; }
+
+        [Required]
+        [MinLength(1, ErrorMessage = "Industry must be min 1 characters")]
+        [MaxLength(20, ErrorMessage = "Industry cannot be over 20 characters")]
         public string Industry { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1, 5000000000)]
         public long MarketCap { get; set; }
         public List<CommentDto>? Comments { get; set; }
     }
