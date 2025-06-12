38d7f66ca457dbbc20de7d0d0d8abf9559828b4a
Aleksandr Babayan <babayanaleksandr80@gmail.com>
2025-06-13 01:07:40 +0400

    feat: Add Pagination to Stocks

--- Repositories/StockRepository.cs
+++ Repositories/StockRepository.cs
@@ -48,7 +48,9 @@ namespace api.Repositories
                 }
             }
 
-            return await stocks.ToListAsync();
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
 
         public async Task<Stock?> FindByIdAsync(int id)
